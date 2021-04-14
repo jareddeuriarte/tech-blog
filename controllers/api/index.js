23b1c27f497caab32163b914b1950885d367adba
Jared de Uriarte <jared.deuri@gmail.com>
2021-04-13 23:33:22 -0400

    a lot of boilerplate

---
+++ controllers/api/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const  = require('./');
+const  = require('./');
+
+router.use('/', );
+router.use('/api', );
+
+module.exports = router;
---
+++ controllers/api/routes.js
@@ -0,0 +1,6 @@
+
+const router = require('express').Router();
+// import my models here const {} = require('../models');
+
+
+router.get('/')
